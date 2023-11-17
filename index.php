6164e35fc026eeccc45452c02ade390095b78e76
othmansalaheddine <othmansalaheddine@gmail.com>
2023-11-17 19:52:58 +0100

    modif

--- index.php
+++ index.php
@@ -1,33 +1,41 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="shortcut icon" type="x-icon" href="asset/png/EE.png">
+    <link rel="shortcut icon" type="x-icon" href="assets/png/EE.png">
    <title>LOG-IN</title>
 </head>
-<body style="background-image: url(asset/png/bg.png);">
+<body style="background-image: url(assets/png/bg.png);">
 <div class="container-fluid">
-    <img class="img-fluid" src="asset/svg/LOGO.svg" alt="" style="margin-top: 2rem;margin-left: 36%;">
+    <img class="img-fluid" src="assets/svg/LOGO.svg" alt="" style="margin-top: 2rem;margin-left: 36%;">
 
 </div>
-<form style="background-color:  rgba(0, 0, 0, 76%);;border-radius: 5%;  " class="position-absolute top-50 start-50 translate-middle">
+<form  action="verification.php" method="POST" style="background-color:  rgba(0, 0, 0, 76%);;border-radius: 5%;  " class="position-absolute top-50 start-50 translate-middle">
     <div class="container-fluid">
         <h1 style="font-family: 'stretch pro';color: aliceblue; padding-left: 12rem; padding-right: 12rem ; margin-top: 3rem;" class="h1">LOOG IN</h1>
     </div>
     <div class="input-group mb-3 container-sm" style="margin-top: 6rem;max-width: 60%;height: 3rem; ">
         <span class="input-group-text" id="basic-addon1">@</span>
-        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+        <input type="text" class="form-control" placeholder="username" name="username"aria-label="Username" aria-describedby="basic-addon1">
       </div>
       <div class="input-group mb-3 container-sm" style="margin-top: 3rem;max-width: 60%;height: 3rem; ">
         <span class="input-group-text" id="basic-addon1" >*</span>
-        <input type="password" class="form-control" placeholder="Password"  id="exampleInputPassword1" aria-label="Username" aria-describedby="basic-addon1">
+        <input type="password" class="form-control" placeholder="password" name="password" id="exampleInputPassword1" aria-label="Username" aria-describedby="basic-addon1">
       </div>
     
     <div class="container-fluid" style="margin-left: 17rem;margin-top: 4rem;">
-    <a href=""><button  type="submit" class="btn  " style="margin-bottom: 2rem; align-self: center; background-color: #DBFF00;width: 18%;" >log in</button></a>
+    <a href=""><button  type="submit" id='submit' value='LOGIN' class="btn  " style="margin-bottom: 2rem; align-self: center; background-color: #DBFF00;width: 18%;" >log in</button></a>
 </div>
+<?php
+ if(isset($_GET['erreur'])){
+ $err = $_GET['erreur'];
+ if($err==1 || $err==2)
+ echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+ }
+ ?>
   </form>
 
 
---
+++ profile.php
@@ -0,0 +1,155 @@
+<?php
+	session_start();
+	if(isset($_SESSION['uid'])){
+		header('location:profile.php');
+	}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Amaclone</title>
+	<link rel="stylesheet" type="text/css" href="http://kenwheeler.github.io/slick/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="http://kenwheeler.github.io/slick/slick/slick-theme.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap-3.3.6-dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+<!--
+
+	<div class="navbar navbar-default navbar-fixed-top" id="topnav">
+		<div class="container-fluid">
+			
+
+			
+
+			<ul class="nav navbar-nav navbar-right">
+				<li id='shoppingcart'><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>Cart <span class="badge">0</span>	</a>
+					<div class="dropdown-menu" style="width: 400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">S. No.</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in $</div>
+								</div>
+							</div>
+							<div class="panel-body"></div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+		<br><br><br>
+	<!-- Slider Begins -->
+
+	 <div class="one-time">
+	    <div><img src="assets/images/couverture1.png"></div>
+	    <div><img src="assets/images/couverture2.png"></div>
+		<div><img src="assets/images/couverture3.png"></div>
+
+  	</div>
+
+	<!-- Slider ends -->
+
+	<br>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-2">
+			<div id="get_cat"></div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Categories</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div>-->
+				<div id="get_brand"></div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Brands</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div>-->
+			</div>
+			<div class="col-md-8">
+			<div class="row">
+					<div class="col-md-12" id="cartmsg">
+
+					</div>
+				</div>
+				<div class="panel panel-info">
+					<div class="panel-heading text-center"> Nos produits
+						<div class='pull-right'>
+								Produit sur stock: &nbsp;&nbsp;&nbsp;<a href="#" id='price_sort'>Stock</a> | <a href="#" id='pop_sort'>Popularity</a>
+							</div>
+					</div>
+					<div class="panel-body">
+					<div id="get_product"></div>
+						<!--<div class="col-md-4">
+							<div class="panel panel-info">
+								<div class="panel-heading">Samsung Galaxy</div>
+								<div class="panel-body"><img src="assets/prod_images/samsung_galaxy.jpg"></div>
+								<div class="panel-heading">$500.00
+								<button class="btn btn-danger btn-xs" style="float:right;">Add to Cart</button>
+								</div>
+							</div>
+						</div>-->
+					</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<center>
+					<ul class='pagination' id='pageno'>
+
+					</ul>
+				</center>
+			</div>
+
+			<!-- Modal -->
+
+				<div class="modal fade" id="prod_detail" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				  <div class="modal-dialog" role="document">
+				    <div class="modal-content">
+				      <div class="modal-header">
+				        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				        <h4 class="modal-title" id="myModalLabel">Product Details</h4>
+				      </div>
+				      <div class="modal-body" id='dynamic_content'>
+				        ...
+				      </div>
+				      <div class="modal-footer">
+				        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+				      </div>
+				    </div>
+				  </div>
+				</div>
+
+			 <!-- Modal ends-->
+		</div>
+	</div>
+
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  	<script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.min.js"></script>
+	<script src="assets/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+	<script src="main.js"></script>
+</body>
+
+
+<style> .foot{text-align: center;}
+</style>
+</html>
